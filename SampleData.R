eaee86f79ec06e2c32d11cd178af55700979514b
Bamini Balaji <bbamini@gmail.com>
2018-03-06 16:14:20 -0800

    Updated scripts and resources

--- SampleData.R
+++ SampleData.R
@@ -10,19 +10,23 @@ news.con <- file('en_US.news.txt','rb')
 news <- readLines(news.con, encoding = "UTF-8")
 close(news.con)
 
+blogs <- iconv(blogs, "UTF-8", "ASCII", sub="")
+twitter <- iconv(twitter, "UTF-8", "ASCII", sub="")
+news <- iconv(news, "UTF-8", "ASCII", sub="")
+
 # Sampling ~5000 lines of each of the data sets
 set.seed(123)
-bset <- rbinom(length(blogs), 1, 0.005)
+bset <- rbinom(length(blogs), 1, 0.25)
 sampleb <- blogs[which(bset %in% 1)]
 
 set.seed(987)
-tset <- rbinom(length(twitter), 1, 0.002)
+tset <- rbinom(length(twitter), 1, 0.25)
 samplet <- twitter[which(tset %in% 1)]
 
 set.seed(106)
-nset <- rbinom(length(news), 1, 0.005)
+nset <- rbinom(length(news), 1, 0.25)
 samplen <- news[which(nset %in% 1)]
 
 # Combining sub-sample from blogs, twitter and news
 sample <- c(sampleb, samplet, samplen)
-writeLines(sample, "C:/Users/bamini/Documents/Coursera/Capstone/sample.txt", useBytes = TRUE)
+writeLines(sample, "C:/Users/bamini/Documents/Coursera/Capstone/final/en_US/sample25.txt", useBytes = TRUE)
