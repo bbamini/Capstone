69de8802470b9064d07d252d816afd81503296ed
Bamini Balaji <bbamini@gmail.com>
2018-04-22 00:51:22 -0700

    Modified shiny app to provide 3 predictions

--- Shiny/server.R
+++ Shiny/server.R
@@ -17,42 +17,59 @@ shinyServer(function(input, output, session) {
   
   dbase <- readRDS("C:/Users/bamini/Documents/Coursera/Capstone/final/en_US/Rev/dbasesmall.rds")
   
+
   nextword <- function(phrase) {
     newphrase <- gsub("\\s+", " ", str_trim(gsub("(?!')[[:punct:]]", "", phrase, perl = TRUE)))
     newphrase <- tolower(newphrase)
     last3words <- word(newphrase, -3, -1)
     last2words <- word(newphrase, -2, -1)
     lastword <- word(newphrase, -1)
-    if (!!length(dbase[base == last3words, predicted])) {
-      nextwd <- dbase[base == last3words, predicted]
-      return(nextwd)
-    } else if (!!length(dbase[base == last2words, predicted])) {
-      nextwd <- dbase[base == last2words, predicted]
-      return(nextwd)
-    } else if (!!length(dbase[base == lastword, predicted])) {
-      nextwd <- dbase[base == lastword, predicted]
-      return(nextwd)
-    } else if (!length(dbase[base == lastword, predicted])) {
-      print("No Suggestions")
-    }
+
+    tetraword <- dbase[base == last3words, predicted]
+    triword <- dbase[base == last2words, predicted]
+    biword <- dbase[base == lastword, predicted]
+
+    nextwds <- unique(c(biword, triword, tetraword))
+    return(nextwds)
   }
   
   
-  observe({
+  
+  observeEvent(input$predict, {
+    if (!is.na(nextword(input$text1)[1])) {
+      output$textpred1 <- renderText(isolate(nextword(input$text1)[1]))
+    } else { output$textpred1 <- renderText("")}
     
-    x <- input$text1
-    wordpred <- reactive({
-      nextword(x)
-    })  
+    if (!is.na(nextword(input$text1)[2])) {
+      output$textpred2 <- renderText(isolate(nextword(input$text1)[2]))
+    } else { output$textpred2 <- renderText("")}
     
-    observeEvent(input$predict,{
-      output$textpred <- renderText({wordpred()})  
-    })
-    
-    observeEvent(input$action, {
-      updateTextInput(session, "text1", value = isolate(paste(x, {wordpred()})))
-    })
-    
+    if (!is.na(nextword(input$text1)[3])) {
+      output$textpred3 <- renderText(isolate(nextword(input$text1)[3]))
+    } else { output$textpred3 <- renderText("")}
   })
+  
+  observeEvent(input$select1, {
+    if (!is.na(nextword(input$text1)[1])) {
+      newprediction1 <- nextword(input$text1)[1]  
+    } else { newprediction1 <- ""}
+    updateTextInput(session, "text1", value = paste(input$text1, newprediction1))
+  })
+  
+  
+  observeEvent(input$select2, {
+    if (!is.na(nextword(input$text1)[2])) {
+      newprediction2 <- nextword(input$text1)[2]  
+    } else { newprediction2 <- ""}
+    updateTextInput(session, "text1", value = paste(input$text1, newprediction2))
+  })
+  
+  observeEvent(input$select3, {
+    if (!is.na(nextword(input$text1)[3])) {
+      newprediction3 <- nextword(input$text1)[3]  
+    } else { newprediction3 <- ""}
+    updateTextInput(session, "text1", value = paste(input$text1, newprediction3))
+  })
+
   
 })
--- Shiny/ui.R
+++ Shiny/ui.R
@@ -14,23 +14,40 @@ shinyUI(fluidPage(
   verticalLayout(
     
     # Application title
-    titlePanel("Predict the Next Word"),
+    titlePanel("Word Prediction"),
     
     h3("Instructions"),
     
+    tags$ol(
+      tags$li("Type a phrase into the Text Input box"),
+      tags$li("Click the Predict button to get up to 3 possible next words."),
+      tags$li("If a prediction matches your choice, click the appropriate Select button to extend your phrase with the prediction.")  
+    ),
+    
 
     # Text Input
-    textInput("text1", label = h3("Text input"), value = "Enter text..."),
+    textInput("text1", label = h3("Text input"), value = ""),
     actionButton("predict", label = "Predict"),
+    br(),
     
     
     wellPanel(
-      h3("Possible next words"),
-      textOutput("textpred")
-    ),
+      h4("Possible next word:"),
+      textOutput("textpred1"),
+      actionButton("select1", label = "Select"),
+      br(),
+      textOutput("textpred2"),
+      actionButton("select2", label = "Select"),
+      br(),
+      textOutput("textpred3"),
+      actionButton("select3", label = "Select"),
+      br(),
+      
+      p("Click the button to update the text with the predicted next word.")
+      
+    )
     
-    actionButton("action", label = "Select"),
-    p("Click the button to update the text with the predicted next word.")
+    
   )
   
 ))
